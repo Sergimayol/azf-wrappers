daeff04431a00f49c56589beeeb47813712a1294
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2024-03-26 12:06:59 +0100

    Fix typos

--- azf_wrappers/http/parser.py
+++ azf_wrappers/http/parser.py
@@ -5,14 +5,14 @@ from typing import Any, Optional, Dict
 
 class ToJSON:
     def __init__(self, content: Optional[Dict[str, Any]] = None):
-        self.content = content if self.content is not None else {}
+        self.content = content if content is not None else {}
 
     def parse_content(self) -> str: return json.dumps(self.content)
 
 
 class ToXML:
     def __init__(self, content: Optional[Dict[str, Any]] = None):
-        self.content = content if self.content is not None else {}
+        self.content = content if content is not None else {}
 
     def _parse_dict(self, parent_element: ET.Element, dictionary: Dict[str, Any]):
         for key, value in dictionary.items():
